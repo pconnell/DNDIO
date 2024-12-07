4ba0efa03e36c2d3f8eab9c9f89b5f356914b43f
pconnell <pconnell89@gmail.com>
2024-12-07 08:18:08 -0700

    Small tweak to commands.py
    Using GRPC relay instance and having it be its own file in the same folder may simplify the run.  This should be very close to working if we can test.

--- src/discord/commands/command.py
+++ src/discord/commands/command.py
@@ -3,19 +3,32 @@ import random as rd
 from argparse import ArgumentError
 
 from src.aio.dndio_parser import parse_str
-from src.aio.parser_protobuf import ParserProto
+# from src.aio.parser_protobuf import ParserProto
 from ..discord_bot import DNDIO
+from relay import grpc_relay
+import json
 
 class Commands(Cog):
     
     async def __init__(self, client: DNDIO):
         self.client = client
+        #give this instance an instance of the relay
+        self.relay = grpc_relay('73.95.249.208','44443','./dndio-tls.crt')
 
     async def parse(self, ctx: Context):
         try:
             await args = parse_str(ctx.message.content[1:])
-            to_send = ParserProto(args, ctx)
-            response = to_send.request(self.client.relay)
+            args = json.loads(args)
+            # to_send = ParserProto(args,ctx)
+            # response = to_send.request(self.client.relay)
+            #use our own relay instance to call and respond
+            response = self.relay.call(
+                cmd=args['command'],
+                subcmd=args['subcommand'],
+                svr=str(ctx.guild.id), #maybe?
+                usr=str(ctx.author), #maybe?
+                args=args
+            ) 
         except Exception as e:
             raise e
         return response
